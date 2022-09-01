80e6f26630c8b49d13143cbffb6c34793583c32b
Fernando Paulino <xizes2@hotmail.com>
2022-09-01 16:53:25 +0200

    correct notFoundPage content, styles

--- src/pages/notFound/NotFoundPage.tsx
+++ src/pages/notFound/NotFoundPage.tsx
@@ -1,7 +1,13 @@
+import NotFoundPageStyled from "./NotFoundPageStyled";
+
 const NotFoundPage = (): JSX.Element => {
   return (
     <>
-      <div>Not Found Page</div>
+      <NotFoundPageStyled>
+        <span>NoOOoOoOOooOo!</span>
+        <img src="images/man-screaming.png" alt="man screaming vector" />
+        <span>Intenta otra vez</span>
+      </NotFoundPageStyled>
     </>
   );
 };
