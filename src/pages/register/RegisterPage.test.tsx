9e8e0fd6913d5a6c03c489446be5835ce64f2d8d
Fernando Paulino <xizes2@hotmail.com>
2022-09-03 10:34:52 +0200

    create login route,  page, test, add wrapper i nregister page test

--- src/pages/register/RegisterPage.test.tsx
+++ src/pages/register/RegisterPage.test.tsx
@@ -1,10 +1,15 @@
 import { render, screen } from "@testing-library/react";
+import Wrapper from "../../utils/Wrapper";
 import RegisterPage from "./RegisterPage";
 
-describe("Given a route '/users/register'", () => {
+describe("Given a route '/register'", () => {
   describe("When it  is accessed", () => {
     test("Then it should show the registerForm", () => {
-      render(<RegisterPage />);
+      render(
+        <Wrapper>
+          <RegisterPage />
+        </Wrapper>
+      );
 
       const registerHeading = screen.getByRole("heading", { name: "Registro" });
 
