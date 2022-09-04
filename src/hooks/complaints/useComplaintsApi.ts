5aefc1feac6d8d78257ea16f7f1d08cd9cfe25d6
Fernando Paulino <xizes2@hotmail.com>
2022-09-04 16:11:12 +0200

    erase modal, destructure data, correct date formt, handlers, test

--- src/hooks/complaints/useComplaintsApi.ts
+++ src/hooks/complaints/useComplaintsApi.ts
@@ -2,20 +2,13 @@ import axios, { AxiosResponse } from "axios";
 import { useCallback } from "react";
 import { toast } from "react-toastify";
 import { getAllComplaintsActionCreator } from "../../app/store/feature/complaints/complaintsSlicer";
-
-import { useAppDispatch, useAppSelector } from "../../app/store/hooks";
+import { useAppDispatch } from "../../app/store/hooks";
 import IRegisteredComplaint from "../../interfaces/complaints/Complaints";
 
 export const loadingModal = (message: string) =>
   toast.loading(message, {
     position: toast.POSITION.TOP_CENTER,
     closeButton: true,
-  });
-
-export const successModal = (message: string) =>
-  toast.success(message, {
-    position: toast.POSITION.TOP_CENTER,
-    autoClose: 2000,
   });
 
 export const errorModal = (error: string) =>
@@ -25,18 +18,27 @@ export const errorModal = (error: string) =>
   });
 
 const useComplaints = () => {
-  const complaints = useAppSelector(({ complaints }) => complaints);
   const dispatch = useAppDispatch();
-
-  const getAllComplaints = useCallback(async () => {
+  const getAllComplaints = useCallback(async (): Promise<void> => {
+    const token = localStorage.getItem("token");
     const url: string = `${process.env.REACT_APP_API_URL}complaints`;
-    loadingModal("Give us a second...");
     try {
-      const { data }: AxiosResponse<IRegisteredComplaint> = await axios.get(
-        url
+      loadingModal("Give us a second...");
+
+      const {
+        data: { complaints },
+      } = await axios.get(url, {
+        headers: { authorization: `Bearer ${token}` },
+      });
+
+      const complaintsList = complaints.map(
+        (complaint: IRegisteredComplaint) => ({
+          ...complaint,
+          creationDate: new Date(complaint.creationDate as Date),
+        })
       );
-      dispatch(getAllComplaintsActionCreator([data]));
-      successModal("Registered with success!");
+
+      dispatch(getAllComplaintsActionCreator(complaintsList));
     } catch (error) {
       errorModal("NoOoOoOoo! Please try again.");
     }
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -32,6 +32,7 @@ export const handlers = [
       );
     }
   ),
+
   rest.post(
     `${process.env.REACT_APP_API_URL}users/login`,
     async (req, res, ctx) => {
@@ -53,6 +54,39 @@ export const handlers = [
             password: "adampass",
           },
         ])
+      );
+    }
+  ),
+
+  rest.get(
+    `${process.env.REACT_APP_API_URL}complaints`,
+    async (req, res, ctx) => {
+      const headerTestError = req.headers.get("IsTestError");
+
+      if (headerTestError) {
+        return res(
+          ctx.status(500),
+          ctx.json({
+            error: "Something went wrong",
+          })
+        );
+      }
+
+      return res(
+        ctx.status(200),
+        ctx.json({
+          complaints: [
+            {
+              category: "fuentes",
+              title: "fuente rota",
+              description: "",
+              countComplaints: 1,
+              image: "fuente.jpg",
+              creationDate: new Date(),
+              id: "987654",
+            },
+          ],
+        })
       );
     }
   ),
