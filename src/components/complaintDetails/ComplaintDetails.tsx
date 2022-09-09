78e821c5503e9453253f216f587fd00dd2f9ba17
Fernando Paulino <xizes2@hotmail.com>
2022-09-09 12:41:19 +0200

    remove map, add state handler for details data

--- src/components/complaintDetails/ComplaintDetails.tsx
+++ src/components/complaintDetails/ComplaintDetails.tsx
@@ -1,25 +1,37 @@
 import { faPenToSquare, faTrashCan } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import useComplaints from "../../hooks/complaints/useComplaintsApi";
+import { IRegisteredComplaint } from "../../interfaces/complaints/Complaints";
 import ComplaintDetailsStyled from "./ComplaintDetailsStyled";
 
 const ComplaintDetails = () => {
+  const complaintInitialState: IRegisteredComplaint = {
+    category: "",
+    title: "",
+    countComplaints: 0,
+    image: "",
+    id: "",
+  };
+
+  const [complaint, setComplaint] = useState(complaintInitialState);
+  const { getComplaint } = useComplaints();
+  const { id } = useParams();
+
+  useEffect(() => {
+    (async () => {
+      const complaint = await getComplaint(id!);
+      setComplaint(complaint);
+    })();
+  }, [getComplaint, id]);
+
   return (
     <ComplaintDetailsStyled>
       <div className="complaint-card">
-        <MapContainer
-          center={[41.390205, 2.154007]}
-          zoom={12}
-          scrollWheelZoom={false}
-        >
-          <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          />
-        </MapContainer>
         <div className="complaint-card__detail-container">
           <h3 className="complaint-card__title">Categoría:</h3>
-          <span className="title-container__text">Contenedor de resíduos</span>
+          <span className="title-container__text">{complaint.category}</span>
         </div>
         <div className="complaint-card__detail-container">
           <h3 className="complaint-card__title">Dirección:</h3>
@@ -29,28 +41,29 @@ const ComplaintDetails = () => {
         </div>
         <div className="complaint-card__detail-container">
           <h3 className="complaint-card__title">Fecha:</h3>
-          <span className="date-container__text">27/08/2022</span>
+          <span className="date-container__text">
+            {complaint.creationDate?.toLocaleDateString()}
+          </span>
         </div>
         <span className="complaint-card__complaint-count">
-          Queja repetida <span>1</span> vez
+          Queja repetida <span>{complaint.countComplaints}</span> vez
         </span>
         <div className="complaint-card__detail-container">
           <h3 className="complaint-card__title">Descripción:</h3>
           <span className="description-container__text">
-            El contenedor de orgánico casi siempre está lleno y por eso las
-            personas dejan la basura fuera. Mucha suciedad alrededor.
+            {complaint.description}
           </span>
         </div>
         <div className="image-container">
           <img
-            src="/images/man-screaming.png"
-            alt={"man screaming"}
+            src={complaint.image}
+            alt={complaint.image}
             className="image-container__image"
             height={"50px"}
           />
           <img
-            src="/images/man-screaming.png"
-            alt={"man screaming"}
+            src={complaint.image}
+            alt={complaint.image}
             className="image-container__image"
             height={"50px"}
           />
