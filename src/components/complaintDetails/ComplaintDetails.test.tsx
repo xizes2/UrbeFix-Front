620817bd96de4c76a0f2d3d41463ca34f96e9272
Fernando Paulino <xizes2@hotmail.com>
2022-09-10 23:03:31 +0200

    create complaintDetails test, add param for map render

---
+++ src/components/complaintDetails/ComplaintDetails.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from "@testing-library/react";
+import Wrapper from "../../utils/Wrapper";
+import ComplaintDetails from "./ComplaintDetails";
+
+describe("Given a ComplaintDetails component", () => {
+  describe("When instantiated", () => {
+    test("Then it should show", () => {
+      render(
+        <Wrapper>
+          <ComplaintDetails />
+        </Wrapper>
+      );
+
+      const textCategory = screen.getByText("Categoría:");
+      const textAddress = screen.getByText("Dirección:");
+      const textDate = screen.getByText("Fecha:");
+      const textDescription = screen.getByText("Descripción:");
+
+      expect(textCategory).toBeInTheDocument();
+      expect(textAddress).toBeInTheDocument();
+      expect(textDate).toBeInTheDocument();
+      expect(textDescription).toBeInTheDocument();
+    });
+  });
+});
--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -5,9 +5,11 @@ const Map = (): JSX.Element => {
   const mapsKey = process.env.REACT_APP_MAPS_API_KEY;
   const isUserGeolocationActive = navigator.geolocation;
   const barcelonaCoordinates = [41.390205, 2.154007];
+
   const [lat, setLat] = useState<number>();
   const [lng, setLng] = useState<number>();
   const [status, setStatus] = useState<boolean>();
+
   const getLocation = useCallback(() => {
     if (!isUserGeolocationActive) {
       setStatus(false);
@@ -20,7 +22,8 @@ const Map = (): JSX.Element => {
         },
         () => {
           setStatus(false);
-        }
+        },
+        { enableHighAccuracy: true }
       );
     }
   }, [isUserGeolocationActive]);
@@ -35,9 +38,7 @@ const Map = (): JSX.Element => {
       height="250"
       style={{ border: 0 }}
       src={`https://www.google.com/maps/embed/v1/place?key=${mapsKey}&q=${
-        status
-          ? [lat, lng] + `&zoom=16`
-          : { bcnCoordinates: barcelonaCoordinates } + `&zoom=11`
+        status ? [lat, lng] + `&zoom=16` : { barcelonaCoordinates } + `&zoom=11`
       }`}
       allowFullScreen={false}
     ></MapStyled>
