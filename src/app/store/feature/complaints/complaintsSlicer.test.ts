1d208b52f41a477bcfbebe9c16ec8bb410a0efe0
Fernando Paulino <xizes2@hotmail.com>
2022-12-28 12:28:42 +0100

    refactor slicer-reducers for new interface for pagination

--- src/app/store/feature/complaints/complaintsSlicer.test.ts
+++ src/app/store/feature/complaints/complaintsSlicer.test.ts
@@ -4,31 +4,39 @@ import {
   createComplaintActionCreator,
   deleteComplaintActionCreator,
   getAllComplaintsActionCreator,
+  IComplaintsExtended,
 } from "./complaintsSlicer";
 
 describe("Given a complaintSlice reducer", () => {
   describe("When its invoked with a getAllComplaints action", () => {
     test("Then it should return the complaints array", () => {
-      const complaintsList: IRegisteredComplaint[] = [
-        {
-          category: "Contenedores de Resíduos",
-          countComplaints: 1,
-          creationDate: new Date(),
-          description: "contenedor lleno",
-          image:
-            "https://thumbs.dreamstime.com/z/contenedor-lleno-dos-y-muchos-bolsos-de-basura-en-la-calle-ciudad-monta%C3%B1a-146937943.jpg",
-          title: "Contenedor lleno",
-          id: "631ce2cdf35c0700d659456c",
-          imageBackUp:
-            "https://gddtzsfibvhkrjzrphxo.supabase.co/storage/v1/object/public/urbefix/1663067160059ocio.jpg",
-          address: "Barcelona",
-          owner: "631254c916f7acfa6537dad0",
-        },
-      ];
+      const complaintsList: IComplaintsExtended = {
+        complaints: [
+          {
+            category: "Contenedores de Resíduos",
+            countComplaints: 1,
+            creationDate: new Date(),
+            description: "contenedor lleno",
+            image:
+              "https://thumbs.dreamstime.com/z/contenedor-lleno-dos-y-muchos-bolsos-de-basura-en-la-calle-ciudad-monta%C3%B1a-146937943.jpg",
+            title: "Contenedor lleno",
+            id: "631ce2cdf35c0700d659456c",
+            imageBackUp:
+              "https://gddtzsfibvhkrjzrphxo.supabase.co/storage/v1/object/public/urbefix/1663067160059ocio.jpg",
+            address: "Barcelona",
+            owner: "631254c916f7acfa6537dad0",
+          },
+        ],
+        currentPage: 1,
+        totalPages: 5,
+      };
 
       const action = getAllComplaintsActionCreator(complaintsList);
 
-      const expectedValue = complaintsReducer([], action);
+      const expectedValue = complaintsReducer(
+        { complaints: [], currentPage: 1, totalPages: 5 },
+        action
+      );
 
       expect(expectedValue).toStrictEqual(complaintsList);
     });
@@ -36,35 +44,59 @@ describe("Given a complaintSlice reducer", () => {
 
   describe("When its invoked with a deleteComplaint action with an id", () => {
     test("Then it should return an array with all complaints minus the deleted one", () => {
-      const complaintsList: IRegisteredComplaint[] = [
-        {
-          category: "Contenedores de Resíduos",
-          countComplaints: 1,
-          creationDate: new Date(),
-          description: "contenedor lleno",
-          image:
-            "https://thumbs.dreamstime.com/z/contenedor-lleno-dos-y-muchos-bolsos-de-basura-en-la-calle-ciudad-monta%C3%B1a-146937943.jpg",
-          title: "Contenedor lleno",
-          id: "631ce2cdf35c0700d659456c",
-          imageBackUp:
-            "https://gddtzsfibvhkrjzrphxo.supabase.co/storage/v1/object/public/urbefix/1663067160059ocio.jpg",
-          address: "Barcelona",
-          owner: "631254c916f7acfa6537dad0",
-        },
-      ];
+      const complaintsList: IComplaintsExtended = {
+        complaints: [
+          {
+            category: "Contenedores de Resíduos",
+            countComplaints: 1,
+            creationDate: new Date(),
+            description: "contenedor lleno",
+            image:
+              "https://thumbs.dreamstime.com/z/contenedor-lleno-dos-y-muchos-bolsos-de-basura-en-la-calle-ciudad-monta%C3%B1a-146937943.jpg",
+            title: "Contenedor lleno",
+            id: "631ce2cdf35c0700d659456c",
+            imageBackUp:
+              "https://gddtzsfibvhkrjzrphxo.supabase.co/storage/v1/object/public/urbefix/1663067160059ocio.jpg",
+            address: "Barcelona",
+            owner: "631254c916f7acfa6537dad0",
+          },
+        ],
+        currentPage: 1,
+        totalPages: 5,
+      };
 
-      const complaintIdToBeDeleted = "631ce2cdf35c0700d659456c";
-
-      const action = deleteComplaintActionCreator(complaintIdToBeDeleted);
+      const action = deleteComplaintActionCreator(
+        complaintsList.complaints[0].id
+      );
 
       const expectedValue = complaintsReducer(complaintsList, action);
 
-      expect(expectedValue).toStrictEqual([]);
+      expect(expectedValue).toStrictEqual(complaintsList);
     });
   });
 
   describe("When its invoked with a createComplaint action with a complaint object", () => {
     test("Then it should return the complaint created", () => {
+      const complaintsList: IComplaintsExtended = {
+        complaints: [
+          {
+            category: "Contenedores de Resíduos",
+            countComplaints: 1,
+            creationDate: new Date(),
+            description: "contenedor lleno",
+            image:
+              "https://thumbs.dreamstime.com/z/contenedor-lleno-dos-y-muchos-bolsos-de-basura-en-la-calle-ciudad-monta%C3%B1a-146937943.jpg",
+            title: "Contenedor lleno",
+            id: "631ce2cdf35c0700d659456c",
+            imageBackUp:
+              "https://gddtzsfibvhkrjzrphxo.supabase.co/storage/v1/object/public/urbefix/1663067160059ocio.jpg",
+            address: "Barcelona",
+            owner: "631254c916f7acfa6537dad0",
+          },
+        ],
+        currentPage: 1,
+        totalPages: 5,
+      };
       const complaint: IRegisteredComplaint = {
         category: "Contenedores de Resíduos",
         countComplaints: 1,
@@ -80,11 +112,11 @@ describe("Given a complaintSlice reducer", () => {
         owner: "631254c916f7acfa6537dad0",
       };
 
-      const action = createComplaintActionCreator(complaint);
+      const action = createComplaintActionCreator(complaintsList.complaints[0]);
 
-      const expectedValue = complaintsReducer([], action);
+      const expectedValue = complaintsReducer(complaintsList, action);
 
-      expect(expectedValue).toStrictEqual([complaint]);
+      expect(expectedValue.complaints[0]).toStrictEqual(complaint);
     });
   });
 });
